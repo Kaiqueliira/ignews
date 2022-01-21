099dc71ad5a7ccd6c2bb8b28aa50f8f03e0488a7
Kaique Lira <Kaiqueliira@gmail.com>
2022-01-21 17:15:06 -0300

    Conexão com o Stripe pegando informações do valor do produto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 import Head from "next/head";
+import { GetServerSideProps } from "next";
+import { stripe } from "../services/stripe";
 import { SubscribeButton } from "../components/SubscribeButton";
 
 import styles from "./home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -18,10 +27,10 @@ export default function Home() {
           <p>
             Get Access to all the publications
             <br />
-            <span>for $9,90 month</span>
+            <span>for {product.amount} and month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl Coding" />
@@ -29,3 +38,21 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1KKOLWGsxAu29tvIkk4aspPq");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-Us", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
